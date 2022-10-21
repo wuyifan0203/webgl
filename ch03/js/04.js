080e814944997d3b72805a5e0258a9c8108de251
wuyifan <wuyifan@max-optics.com>
2022-10-21 15:07:02 +0800

    feat:add rotate

--- ch03/js/04.js
+++ ch03/js/04.js
@@ -1,15 +1,21 @@
+import {angle2Radians} from "../../lib/math.js";
+
+// x' = x * cosB - y * sinB
+// y' = x * sinB + y * cosB
+
 const vShader =
-    'attribute vec4 a_Position;\n'+
-    'uniform vec4 u_Position;\n'+
-    'void main() {\n'+
-    '  gl_Position = a_Position + u_Position;\n'+
+    'attribute vec4 a_Position;\n' +
+    'uniform float u_Cos, u_Sin;\n' +
+    'void main() {\n' +
+    '  gl_Position.x = a_Position.x * u_Cos - a_Position.y * u_Sin;\n' +
+    '  gl_Position.y = a_Position.x * u_Sin + a_Position.y * u_Cos;\n' +
+    '  gl_Position.z = a_Position.z;\n' +
+    '  gl_Position.w = 1.0;\n' +
     '}\n';
 
 const fShader =
-    'precision mediump float;\n'+
-    'uniform vec4 u_Color;\n'+
-    'void main() {\n'+
-    '  gl_FragColor = u_Color;\n'+
+    'void main() {\n' +
+    '  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n' +
     '}\n';
 
 function main() {
@@ -21,20 +27,31 @@ function main() {
     const origin = [
         -2,-2,2,-2,0,2
     ]
+    const n = initVertexBuffers(origin,gl)
 
+
+    const angle = 90.0;
+    const raians = angle2Radians(angle);
+    const [u_sin,u_cos] = [Math.sin(raians),Math.cos(raians)];
+
+    const u_Sin = gl.getUniformLocation(gl.program,'u_Sin');
+    const u_Cos = gl.getUniformLocation(gl.program,'u_Cos');
+
+    gl.uniform1f(u_Sin,u_sin);
+    gl.uniform1f(u_Cos,u_cos);
+
+
+    gl.clearColor(0.0,0.0,0.0,1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    gl.drawArrays(gl.TRIANGLES,0,n);
+}
+
+function initVertexBuffers(origin,gl){
     const normalise = origin.map(i=>i/10);
-
     const vertices = new Float32Array(normalise);
-
-    const a_Position = gl.getAttribLocation(gl.program,'a_Position');
-
-    const u_Position = gl.getUniformLocation(gl.program,'u_Position');
-
-    const position = [1.0,1.0,1.0].map(i=>i/10)
-
-    gl.uniform4f(u_Position,position[0],position[1],position[2],0.0)
-
     const n = vertices.length / 2;
+    const a_Position = gl.getAttribLocation(gl.program,'a_Position');
 
     // 创建顶点缓冲区
     const verticesBuffer = gl.createBuffer();
@@ -50,11 +67,8 @@ function main() {
 
     // 启用 Attribute
     gl.enableVertexAttribArray(a_Position)
+    return n;
 
-    gl.clearColor(0.0,0.0,0.0,1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT)
-
-    gl.drawArrays(gl.TRIANGLES,0,n);
 }
 
 
